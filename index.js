dd8323b1137f4ab15dc9d39c63e9f815976c7602
BanjoInertia <nozembro@gmail.com>
2025-04-18 05:08:49 -0300

    css fixes

--- index.js
+++ index.js
@@ -171,9 +171,9 @@ const product = [
 
 function getItemsPerPage() {
     const width = window.innerWidth;
-    if (width <= 600) return 1;
-    if (width <= 900) return 2;
+    if (width <= 800) return 2;
     if (width <= 1200) return 3;
+    if (width <= 1350) return 4;
     return 5;
 }
 
@@ -257,7 +257,6 @@ function categoriesDropdownSubmenu() {
 
 document.addEventListener('DOMContentLoaded', categoriesDropdownSubmenu);
 
-
 class ProductSlider {
     constructor(wrapperElement) {
         this.wrapper = wrapperElement;
@@ -308,7 +307,7 @@ class ProductSlider {
                     <span class="new-tag">Novo</span>
                     <img class="product-thumb" src="${item.image}" alt="">
                 </div>
-                <div class="product-info">
+                <div>
                     <h3 class="product-short-description">${item.description}</h3>
                     <div class="price-discount-container">
                         <div class="price-container">
@@ -350,3 +349,50 @@ document.addEventListener('DOMContentLoaded', () => {
         new ProductSlider(wrapper);
     });
 });
+
+const form = document.getElementById('newsletterForm')
+const newsletterName = document.getElementById('newsletterName')
+const newsletterEmail = document.getElementById('newsletterEmail')
+const newsletterCheck = document.getElementById('newsletterCheck')
+const errorMessages = document.querySelectorAll('.error-message')
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    console.log('eu sou humilde')
+
+    let isValid = true;
+
+    errorMessages.forEach(msg => {
+        msg.textContent = '';
+        msg.style.display = 'none';
+      });
+
+    if (newsletterName.value.trim() === '') {
+        showError(newsletterName, 'Name is required');
+        isValid = false;
+    }
+
+    if (!newsletterEmail.value.match(/^\S+@\S+\.\S+$/)) {
+        showError(newsletterEmail, 'Enter a valid email');
+        isValid = false;
+    }
+
+    if (!newsletterCheck.checked) {
+        showError(newsletterCheck, 'You must agree to continue.');
+        isValid = false;
+    }
+
+    if (isValid) {
+        alert('Subscription successful!');
+        form.reset();
+    }
+});
+
+function showError(inputElement, message) {
+    const error = inputElement.nextElementSibling;
+    error.textContent = message;
+    error.style.display = 'block';
+}
+
+
